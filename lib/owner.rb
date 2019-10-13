1eb3fd804edb1491e625d60a6994f258d7e9baef
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-13 22:51:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+  @@all = []
+  
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def self.all 
+    return @@all
+  end
+  
+  def self.count
+    return @@all.count
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def buy_cat
+    Cat.new(name, self)
+  end
+  
 end
