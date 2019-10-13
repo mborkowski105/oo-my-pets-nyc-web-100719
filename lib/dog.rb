1b09d19c426429b61e46247a83026712861a1dff
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-13 22:59:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
   # code goes here
+  @@all = []
+  
+  attr_accessor :owner
+  attr_reader :name, :mood
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all 
+    return @@all
+  end
+  
+  def mood=(mood)
+    @mood = mood
+  end
 end
